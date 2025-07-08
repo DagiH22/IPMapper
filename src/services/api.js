9617d7e23732acd7fe5d0d7598f9e6ddc74d7550
Dagi <dagiheywot@gmail.com>
2025-07-08 11:52:48 +0300

    made search with domain to work with goole dns over https(DoH) api

--- src/services/api.js
+++ src/services/api.js
@@ -1,33 +1,49 @@
 const API_KEY = import.meta.env.VITE_IP_API_KEY
 
-async function api({type,input}) {
+async function  api({type,input}) {
   let uri = ''
+  let theIP = ''
   if(type === 'ip'){
      uri = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}${input ? `&ipAddress=${input}` : ''}`
   }
-  else{
-     uri = `https://geo.ipify.org/api/v2/country?apiKey=${API_KEY}&domain=${input}`
-  }
-    
+  else {
+    const googleResolve = `https://dns.google/resolve?name=${input}&type=A`
+    console.log('reached here')
     try {
-        const res = await fetch(uri)
-        if (!res.ok) {
-          const error = new Error(`Failed to fetch data`)
-          error.status = res.status
-          error.statusText = res.statusText
-          error.url = uri
-          throw error
-        }
-        return await res.json()
-      } catch (err) {
-        if (!err.status) {
-          err.message = 'Network error: Failed to fetch (maybe CORS or no internet)'
-          err.status = 'N/A'
-          err.statusText = 'Unknown'
-          err.url = uri
-        }
-        throw err
+      const res = await fetch(googleResolve)
+      const d = await res.json()
+      if (d.Answer && d.Answer.length > 0) {
+        theIP = d.Answer[0].data
+        console.log(theIP)
+      } else {
+        throw new Error('No A record found for domain')
       }
+    } catch (err) {
+      console.log('DNS resolve error:', err.message)
+      throw err
+    }
+    uri = `https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${theIP}`
+  }  
+  
+  try {
+      const res = await fetch(uri)
+      if (!res.ok) {
+        const error = new Error(`Failed to fetch data`)
+        error.status = res.status
+        error.statusText = res.statusText
+        error.url = uri
+        throw error
+      }
+      return await res.json()
+    } catch (err) {
+      if (!err.status) {
+        err.message = 'Network error: Failed to fetch (maybe CORS or no internet)'
+        err.status = 'N/A'
+        err.statusText = 'Unknown'
+        err.url = uri
+      }
+      throw err
+    }
 }
 
 export default api
